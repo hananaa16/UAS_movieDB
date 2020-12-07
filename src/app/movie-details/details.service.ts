fbeab0688a59e7494f513b17f9b14426ad317ca6
Tania Irena Gunawan <75471765+tanikohiranuma@users.noreply.github.com>
2020-12-07 22:17:08 +0700

    Update details.service.ts

--- src/app/movie-details/details.service.ts
+++ src/app/movie-details/details.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router }   from '@angular/router';
+import { MoviesService }            from '../movies/movies.service';
+import { Movie }                    from '../movies/movie';
+import {Location} from '@angular/common';
 
-@Injectable({
-  providedIn: 'root'
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
 })
-export class DetailsService {
+export class DetailsComponent implements OnInit {
+  selectedMovie: Movie;
+  errorMessage: string;
+  language: string;
 
-  constructor() { }
+  constructor(
+    private moviesService: MoviesService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.language = this.moviesService.getLanguage();
+    this.route.params.subscribe(
+      params => {
+        let id = params['id'];
+        if (id) this.getDetails(id);
+      });
+  }
+
+  getDetails(id: number) {
+    this.moviesService.getDetails(id)
+      .subscribe(
+        response => this.selectedMovie = response,
+        error => this.errorMessage = <any>error);
+  }
+
+  back() {
+    this.location.back();
+  }
 }
