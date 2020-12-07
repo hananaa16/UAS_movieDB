09beb32627464a731af76e2bfd7457ea793fa4d1
hananaa16 <75466827+hananaa16@users.noreply.github.com>
2020-12-07 22:58:40 +0700

    Update movie-now-playing.component.ts

--- src/app/movie-now-playing/movie-now-playing.component.ts
+++ src/app/movie-now-playing/movie-now-playing.component.ts
@@ -1,29 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }                   from '@angular/router';
-import { Observable }               from 'rxjs/Observable';
-import { NowPlayingService }            from './now-playing.service';
-import { NowPlaying }                    from './now-playing';
 
 @Component({
-  selector: 'app-movie-now-playing',
-  templateUrl: './movie-now-playing.component.html',
-  styleUrls: ['./movie-now-playing.component.css']
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
 })
-export class MovieNowPlayingComponent implements OnInit {
+export class AboutUsComponent implements OnInit {
 
-  constructor(private nowplayingService: NowPlayingService,
-    private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.language = this.nowplayingService.getLanguage();
-    this.getMovies();
-  }
-   getMovies() {
-    this.movies = this.nowplayingService.getMovies();
-  }
-  
-  onSelect(nowplaying: NowPlaying) {
-    this.router.navigate(['./../now-playing', movie.id]);
   }
 
 }
