d9ff3ad547e4b92d8583f652ba708edbc28d2e32
hananaa16 <75466827+hananaa16@users.noreply.github.com>
2020-12-07 21:58:43 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieNowPlayingComponent } from './movie-now-playing/movie-now-playing.component';
+import { MovieUpcomingComponent } from './movie-upcoming/pengurus-list.component';
+import { MovieRecommendationComponent } from './movie-recommendation/movie-recommendation.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movie-details', component: MovieDetailsComponent },
+  { path: 'movie-now-playing', component: MovieNowPlayingComponent },
+  { path: 'movie-upcoming', component: MovieUpcomingComponent },
+  { path: 'movie-recommendation', component: MovieRecommendationComponent },
+  { path: 'about-us', component: AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
