e390cbcca0c9b3a91c48c7fa2b7fb1d3e0ac3913
anasden26 <75472769+anasden26@users.noreply.github.com>
2020-12-10 15:04:56 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "./components/home/Home";
 import { MovieUpcoming } from "./components/upcoming/Upcoming";
 import { NowPlaying } from "./components/nowplaying/NowPlaying";
-import { MovieDetail } from "./components/moviedetail/MovieDetail";
+import { PopularMovie } from "./components/popular/Popular";
 
 export function App() {
   return (
@@ -12,8 +12,8 @@ export function App() {
       <Switch>
         <Route path="/" component={ Home } exact />
         <Route path="/nowplaying/NowPlaying" component={ NowPlaying } />
+        <Route path="/popular/Popular" component={ PopularMovie } />
         <Route path="/upcoming/Upcoming" component={ MovieUpcoming } />
-        <Route path="/movie/:id" component={MovieDetail} />
       </Switch>
     </main>
   );
