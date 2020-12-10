3676ea39b9112448ed5569adf6768d4405ab37e0
anasden26 <75472769+anasden26@users.noreply.github.com>
2020-12-10 15:03:38 +0700

    Add files via upload

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { fetchMovies, fetchMoviesUpcoming } from "../../service";
+import { fetchMovies, fetchMoviesUpcoming, fetchPopularMovie } from "../../service";
 import RBCarousel from "react-bootstrap-carousel";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import { Link } from "react-router-dom";
@@ -8,11 +8,13 @@ import { Nav } from 'react-bootstrap';
 
 export function Home() {
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
   const [upcoming, setUpcoming] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
       setNowPlaying(await fetchMovies());
+      setPopular(await fetchPopularMovie());
       setUpcoming(await fetchMoviesUpcoming());
     };
 
@@ -56,6 +58,27 @@ export function Home() {
     );
   });
 
+  const pop = popular.slice(0,6).map((item, index) => {
+    return (
+        <div className="col-md-2 col-sm-6" key={index}>
+            <div className="card">
+                <Link to={`/movie/${item.id}`}>
+                <img className="img-fluid" src={item.poster} alt={item.title}></img>
+                </Link>
+            </div>
+            <div className="mt-3">
+                <p style={{ fontWeight: "bolder "}}>{item.title}</p>
+                <p>Rated: {item.rating}</p>
+                <ReactStars
+                count={item.rating}
+                size={20}
+                color1={"#f4c10f"}
+                ></ReactStars>
+            </div>
+        </div>
+    );
+  });
+
   const up = upcoming.slice(0,6).map((item, index) => {
     return (
       <div className="col-md-2 col-sm-6" key={index}>
@@ -78,6 +101,7 @@ export function Home() {
       <Nav className="ml-auto justify-content-center" defaultActiveKey="/">
         <Nav.Item className="mynav"><Nav.Link href="/">Home</Nav.Link></Nav.Item>
         <Nav.Item className="mynav2"><Nav.Link href="/nowplaying/NowPlaying">Now Playing</Nav.Link></Nav.Item>
+        <Nav.Item className="mynav2"><Nav.Link href="/popular/Popular">Popular</Nav.Link></Nav.Item>
         <Nav.Item className="mynav2"><Nav.Link href="/upcoming/Upcoming">Upcoming</Nav.Link></Nav.Item>
       </Nav><br></br>
       <div className="col">
@@ -98,7 +122,13 @@ export function Home() {
         </div>
       </div>
       <div className="row mt-2">
-        <h4 className="mt-3">Upcoming</h4>
+        <h4 className="mt-3">Popular</h4>
+        <div className="row mt-3">
+          {pop}
+        </div>
+      </div>
+      <div className="row mt-2">
+        <h4 className="mt-4">Upcoming</h4>
         <div className="row mt-3">
           {up}
         </div>
