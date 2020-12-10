3a080b03a3aacff46a58675c6e02ff950f4ea8c2
hananaa16 <75466827+hananaa16@users.noreply.github.com>
2020-12-10 16:27:30 +0700

    Add files via upload

---
+++ src/components/aboutus/AboutUs.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Nav } from 'react-bootstrap';
+import hana from './hana.jpg';
+import denisha from './denisha.jpg';
+import irena from './iren.jpg';
+
+export function AboutUs() {
+
+  return (
+    <div className="container">
+      <h1 className="mt-3" style={{ textAlign: "center"}}>Dream On! Cinema</h1><br></br>
+      <Nav className="ml-auto justify-content-center" defaultActiveKey="/nowplaying/NowPlaying">
+        <Nav.Item className="mynav2"><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+        <Nav.Item className="mynav"><Nav.Link href="/nowplaying/NowPlaying">Now Playing</Nav.Link></Nav.Item>
+        <Nav.Item className="mynav2"><Nav.Link href="/upcoming/Upcoming">Upcoming</Nav.Link></Nav.Item>
+      </Nav><br></br>
+      <h1 style={{textAlign: "center"}}>About Us</h1>
+      <div className="row mt-3 mb-5">
+        <div className="col-md-4" style={{ color: "white", textAlign: "center" }}>
+            <img className="img-fluid" src={hana} alt="hana" style= {{height:"300px"}}></img>
+        </div>
+        <div className="col-md-4" style={{ color: "white", textAlign: "center" }}>
+            <img className="img-fluid" src={irena} alt="irena" style= {{height:"300px"}}></img>
+        </div>
+        <div className="col-md-4" style={{ color: "white", textAlign: "center" }}>
+        <img className="img-fluid" src={denisha} alt="denisha" style= {{height:"300px"}}></img>
+        </div>
+       </div>
+    
+    </div>
+  );
+}
