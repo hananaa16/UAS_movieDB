3fee093bc15dc6f77dcf28fd8c573f82df696e15
anasden26 <75472769+anasden26@users.noreply.github.com>
2020-12-11 22:18:46 +0700

    Update MovieDetail.jsx

--- src/components/detail/MovieDetail.jsx
+++ src/components/detail/MovieDetail.jsx
@@ -119,7 +119,7 @@ export function MovieDetail({ match }) {
 
   return (
     <div>
-      <Navbar className="col-lg-12 color-nav">
+      <Navbar className="col-lg-12 color-nav" sticky="top">
         <Navbar.Brand href="/">Dream On! Cinema</Navbar.Brand>
         <Nav className="mr-auto justify-content-center" defaultActiveKey="">
           <Nav.Item className="mynav2 ml-3 col-nav"><Nav.Link href="/">Home</Nav.Link></Nav.Item>
