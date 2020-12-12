4657cf6a2ac347729e24b7b8aa66dd7d8944e8ca
hananaa16 <75466827+hananaa16@users.noreply.github.com>
2020-12-12 21:36:19 +0700

    Update NowPlaying.jsx

--- src/components/nowplaying/NowPlaying.jsx
+++ src/components/nowplaying/NowPlaying.jsx
@@ -19,10 +19,10 @@ export function NowPlaying() {
   const movies = nowPlaying.slice(0,20).map((item, index) => {
     return (
       <div className="col-md-3 col-sm-6 d-flex" key={index}>
-        <div className="card mt-4 flex-fill">
+        <div className="card mt-4 flex-fill" style={{backgroundColor:"white"}}>
       <div className="card">
         <Link to={`/movie/${item.id}`}>
-          <img className="img-fluid" src={item.poster} alt={item.title}></img>
+          <img className="img-fluid" src={item.poster} style={{height:"400px"}} alt={item.title}></img>
         </Link>
       </div>
       <div className="mt-3">
